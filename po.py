4f2cf759eb13cc44d2e9dfc95d4e29a549600b7a
youssef azam <108946956+youssef-azam@users.noreply.github.com>
2024-01-30 13:35:44 +0200

    po.py

--- po.py
+++ po.py
@@ -16,4 +16,4 @@ st.markdown(power_bi_html_code, unsafe_allow_html=True)
 
 # Run the Streamlit app
 if __name__ == "__main__":
-    st.write('Hello in my report Powered by Eng:Youssef Azam , for Manger Eng:Salem')
+    st.write('Hello in my report Powered by Eng: Youssef Azam 👨‍💻, for Manager Eng: Salem 🚀')
